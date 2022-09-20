7c1b46a68f2ea11154047d97dfdef0b9f2cff526
itgl <itgel0518@icloud.com>
2022-09-20 16:35:52 +0800

    make it better

--- src/components/Home/buttons/Order.jsx
+++ src/components/Home/buttons/Order.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { checkout } from '../../../api/miniAppApi';
-import { insertOrder, calendarAdd } from '../../../api/amitaApi';
+import { insertOrder, calendarAdd, findUser } from '../../../api/amitaApi';
 
 const saveOrder = (day, time, type, checkoutId, userId, setType, setOrderList, delay) => {
   const date = day.getMonth() + 1 + '/' + day.getDate();
   insertOrder(date, time, type, checkoutId, userId)
     .then((res) => {
       if (res) {
-        const start = new Date(day.getFullYear(), day.getMonth(), day.getDate());
-        const end = new Date(day.getFullYear(), day.getMonth(), day.getDate());
-        const hm = time?.split(':');
-        const dl = delay?.split(':');
-        start.setHours(parseInt(hm[0]));
-        end.setHours(parseInt(hm[0]));
-        start.setMinutes(parseInt(hm[1]));
-        end.setMinutes(parseInt(dl[1]) + parseInt(hm[1]));
-        calendarAdd(start, end, 'test', 'test123');
+        findUser(userId).then((result) => {
+          const start = new Date(day.getFullYear(), day.getMonth(), day.getDate());
+          const end = new Date(day.getFullYear(), day.getMonth(), day.getDate());
+          const hm = time?.split(':');
+          const dl = delay?.split(':');
+          start.setHours(parseInt(hm[0]));
+          end.setHours(parseInt(hm[0]));
+          start.setMinutes(parseInt(hm[1]));
+          end.setMinutes(parseInt(dl[1]) + parseInt(hm[1]));
+          calendarAdd(
+            start,
+            end,
+            result.firstname,
+            'phone: ' + result.phone + (result.gmail ? '\n gmail: ' + result.gmail : '')
+          );
+        });
         setOrderList((i) => [...i, { date: date, hour: time, paid: null, checkoutId: checkoutId }]);
       }
     })
@@ -28,7 +35,6 @@ export const Order = (props) => {
   const { day, time, text, type, userId, setType, setOrderList, delay } = props || {};
   const handleSubmit = () => {
     if (time !== null) {
-      saveOrder(day, time, type, '12321312312', 'admin', setType, setOrderList, delay);
       checkout().then((res) => {
         saveOrder(day, time, type, res.checkoutId, userId, setType, setOrderList, delay);
         window.hpsPayment(res.checkoutId);

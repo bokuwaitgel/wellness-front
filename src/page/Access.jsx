af7b7bacdf9ef072d9f37de72ac7ba13607dfe58
itgl <itgel0518@icloud.com>
2022-09-26 15:44:56 +0800

    test

--- src/page/Access.jsx
+++ src/page/Access.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import { Head } from '../components/Head';
 
 export const Access = () => {
+  var query = window.location.search;
+  console.log(query);
   return (
     <>
       <Head title="Access" description="hello" />
+      {JSON.stringify(query)}
     </>
   );
 };

2248fa2a8455697421247805af115f5d576d1e9e
itgl <itgel0518@icloud.com>
2022-09-30 12:10:35 +0800

    prod

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 export const CLIENT_ID = 'Wallet';
 
+export const setToken = (token) => {
+  if (token) {
+    apiClient.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
+};
+
 export const apiClient = axios.create({
   headers: {
     client_id: CLIENT_ID
@@ -8,13 +14,6 @@ export const apiClient = axios.create({
   baseURL: 'https://test.hipay.mn',
   timeout: 65000
 });
-
-export const setToken = (token) => {
-  if (token) {
-    apiClient.defaults.headers.common.Authorization = `Bearer ${token}`;
-  }
-};
-
 export const getAccessToken = () => {
   return apiClient.defaults.headers.common.Authorization.split(' ')[1];
 };

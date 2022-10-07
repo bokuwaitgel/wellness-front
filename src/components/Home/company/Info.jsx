1f8bf9f92b87b0a0ade022c6e575c8c9ec75654f
itgl <itgel0518@icloud.com>
2022-10-07 16:47:32 +0800

    hayg

--- src/components/Home/company/Info.jsx
+++ src/components/Home/company/Info.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 export const Info = () => {
   return (
     <div className="p-9">
-      <div className="text-left text-base font-semibold">Хаяг</div>
+      <div className="text-left text-base font-semibold">Манай хаяг:</div>
       <div className="text-left text-justify text-xs">
-        Чингэлтэй дүүрэг 5-р хороо Самбуугийн гудамж Твинко барилга 8 давхарт
+        Тэнгис кино театерын автобусны буудалын баруун талд кфс тэй барилгын 8 давхарт. Caffe
+        Bene-тэй хаалгаар ороод лифтээр 7 давхар хүртэл яваад шатаар 1 давхар гарна.
       </div>
       <div className="text-left text-justify text-xs">Холбогдох утас: 99808105</div>
     </div>

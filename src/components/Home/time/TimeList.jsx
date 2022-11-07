0bef9f51545f816558c504029c120095c5b35bca
itgl <itgel0518@icloud.com>
2022-11-07 15:58:22 +0800

    improvement

--- src/components/Home/time/TimeList.jsx
+++ src/components/Home/time/TimeList.jsx
@@ -78,7 +78,6 @@ export const TimeList = (props) => {
       const data = calendarData[i];
       const s = gTimeConcertor(data?.start?.dateTime),
         e = gTimeConcertor(data?.end?.dateTime);
-      console.log(s, e);
       if (s === Start && e === End) return false;
       if (s < Start && e > End) return false;
       if (s < Start && e > Start) return false;
